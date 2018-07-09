1c0c91c9935a67e2f68bc3d1398a7b066f7e06c1
Jonas <ferreirajonasss@gmail.com>
2018-07-09 07:53:50 -0300

    Medidas

---
+++ medidas/main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+
+
+class RootWidget(FloatLayout):
+    pass
+
+
+class MedidaApp(App):
+    
+    def build(self):
+        return RootWidget()
+
+
+MedidaApp().run()
